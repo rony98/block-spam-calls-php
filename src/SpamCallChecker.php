65c8814e7f222b3c8b47a8164cadd87602061fb9
rony98 <verchrony@gmail.com>
2025-09-24 16:04:52 -0400

    Testing behaviour

--- src/SpamCallChecker.php
+++ src/SpamCallChecker.php
@@ -38,7 +38,8 @@ readonly class SpamCallChecker
             $this->setSuccessTwiML();
             $response
                 ->getBody()
-                ->write($this->twiml->asXML());
+                ->("test");
+//                ->write($this->twiml->asXML());
 
             return $response;
         }
